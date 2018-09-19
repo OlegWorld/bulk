524c0ebada2391084885b7faf71c2e769dddd3de
OlegWorld <osmironov@gmail.com>
2018-09-19 12:39:59 +0300

    Splitting into modules

--- test.cpp
+++ test.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <gtest/gtest.h>
 #include "command_handler.h"
 
@@ -27,7 +28,7 @@ TEST(BulkTest, CommandProcessorMultipleTest) {
 }
 
 TEST(BulkTest, CommandReaderTest) {
-    CommandReader reader(1);
+    CommandReader reader(1, std::cin);
     CommandProcessor proc1;
     CommandProcessor proc2;
     reader.subscribe(&proc1);
@@ -41,7 +42,7 @@ TEST(BulkTest, CommandReaderTest) {
 }
 
 TEST(BulkTest, CommandReaderAbortTest) {
-    CommandReader reader(1);
+    CommandReader reader(1, std::cin);
     CommandProcessor proc1;
     CommandProcessor proc2;
     reader.subscribe(&proc1);
@@ -53,6 +54,64 @@ TEST(BulkTest, CommandReaderAbortTest) {
     std::string output = testing::internal::GetCapturedStdout();
 
     ASSERT_EQ(output, "\n\n");
+}
+
+TEST(BulkTest, SingleCommandStringInputTest) {
+    const std::string inputString("command1\ncommand2\ncommand3\n");
+    std::istringstream inputStream(inputString);
+
+    CommandReader reader(1, inputStream);
+    CommandProcessor proc;
+    reader.subscribe(&proc);
+    testing::internal::CaptureStdout();
+    reader.scan_input();
+    std::string output = testing::internal::GetCapturedStdout();
+
+    ASSERT_EQ(output, "bulk: command1\nbulk: command2\nbulk: command3\n\n");
+}
+
+TEST(BulkTest, MulpipleCommandsStringInputTest) {
+    const std::string inputString("command1\ncommand2\ncommand3\n");
+    std::istringstream inputStream(inputString);
+
+    CommandReader reader(3, inputStream);
+    CommandProcessor proc;
+    reader.subscribe(&proc);
+    testing::internal::CaptureStdout();
+    reader.scan_input();
+    std::string output = testing::internal::GetCapturedStdout();
+
+    ASSERT_EQ(output, "bulk: command1, command2, command3\n\n");
+}
+
+TEST(BulkTest, SingleCommandStringInputTestWithTwoProcessors) {
+    const std::string inputString("command1\ncommand2\n");
+    std::istringstream inputStream(inputString);
+
+    CommandReader reader(1, inputStream);
+    CommandProcessor proc1;
+    CommandProcessor proc2;
+    reader.subscribe(&proc1);
+    reader.subscribe(&proc2);
+    testing::internal::CaptureStdout();
+    reader.scan_input();
+    std::string output = testing::internal::GetCapturedStdout();
+
+    ASSERT_EQ(output, "bulk: command1\nbulk: command1\nbulk: command2\nbulk: command2\n\n\n");
+}
+
+TEST(BulkTest, SwitchStateStringInputTest) {
+    const std::string inputString("command1\n{\ncommand2\ncommand3\ncommand4\n}\n");
+    std::istringstream inputStream(inputString);
+
+    CommandReader reader(2, inputStream);
+    CommandProcessor proc;
+    reader.subscribe(&proc);
+    testing::internal::CaptureStdout();
+    reader.scan_input();
+    std::string output = testing::internal::GetCapturedStdout();
+
+    ASSERT_EQ(output, "bulk: command1\nbulk: command2, command3, command4\n\n");
 }
 
 int main(int argc, char **argv)
